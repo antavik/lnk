92c19902bb075841840d860cdf733b02aced725a
antibantique <svecha@protonmail.com>
2023-08-03 20:19:01 +0300

    utils refactoring

--- app/utils.py
+++ app/utils.py
@@ -3,18 +3,20 @@ import re
 from constants import TimeUnit as TU, LNK
 
 
+_TTL_REGEXP = re.compile(
+    f'^(?P<number>\\d+)'
+    f'((?P<{TU.DAYS.value}>d)|'
+    f'(?P<{TU.HOURS.value}>h)|'
+    f'(?P<{TU.MINUTES.value}>m)|'
+    f'(?P<{TU.SECONDS.value}>s))'
+)
+
+
 def parse_ttl(ttl: str) -> tuple[int, TU]:
     number = None
     units = None
-    pattern = (
-        f'^(?P<number>\\d+)'
-        f'((?P<{TU.DAYS.value}>d)|'
-        f'(?P<{TU.HOURS.value}>h)|'
-        f'(?P<{TU.MINUTES.value}>m)|'
-        f'(?P<{TU.SECONDS.value}>s))'
-    )
 
-    match = re.fullmatch(pattern, ttl)
+    match = _TTL_REGEXP.fullmatch(ttl)
     if match is None:
         raise ValueError(f'invalid ttl value: {ttl}')
 
@@ -28,15 +30,16 @@ def parse_ttl(ttl: str) -> tuple[int, TU]:
     return (int(number), TU(units))
 
 
-def calc_seconds(number: int, unit: TU) -> int:
-    seconds_calc = {
-        TU.DAYS: lambda d: d * 24 * 60 * 60,
-        TU.HOURS: lambda h: h * 60 * 60,
-        TU.MINUTES: lambda m: m * 60,
-        TU.SECONDS: lambda s: s,
-    }
+_SECONDS_CALC_MAP = {
+    TU.DAYS: lambda d: d * 24 * 60 * 60,
+    TU.HOURS: lambda h: h * 60 * 60,
+    TU.MINUTES: lambda m: m * 60,
+    TU.SECONDS: lambda s: s,
+}
 
-    calc = seconds_calc.get(unit)
+
+def calc_seconds(number: int, unit: TU) -> int:
+    calc = _SECONDS_CALC_MAP.get(unit)
     if calc is None:
         raise ValueError(f'invalid unit argument value: {unit}')
 
@@ -55,13 +58,17 @@ def clip_task_name(uid: str) -> str:
     return f'clip_{uid}'
 
 
-def str2bool(value: str) -> bool:
-    true_bool_strings = {'yes', 'YES', 'y', 'Y', '1', 'true', 'TRUE', 't', 'T'}
-    false_bool_strings = {'no', 'NO', 'n', 'N', '0', 'false', 'FALSE', 'f', 'F', ''}  # noqa
+_TRUE_BOOL_STRINGS = {'yes', 'YES', 'y', 'Y', '1', 'true', 'TRUE', 't', 'T'}
+_FALSE_BOOL_STRINGS = {'no', 'NO', 'n', 'N', '0', 'false', 'FALSE', 'f', 'F', ''}  # noqa
 
-    if value in true_bool_strings:
+
+def str2bool(value: str) -> bool:
+    if isinstance(value, bool):
+        return value
+
+    if value in _TRUE_BOOL_STRINGS:
         return True
-    elif value in false_bool_strings:
+    elif value in _FALSE_BOOL_STRINGS:
         return False
 
     raise ValueError('unsupported value')
